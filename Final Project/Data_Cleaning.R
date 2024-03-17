4d701cfb9ab45877f288162fc204aea582c4aa6b
Logan Correa <48968193+logancorrea@users.noreply.github.com>
2024-03-17 13:30:58 -0600

    commit

--- Final Project/Data_Cleaning.R
+++ Final Project/Data_Cleaning.R
@@ -1,20 +1,12 @@
 
 library(psych)
+library(dplyr)
 
-
-setwd("~/Documents/GitHub/Prob-and-Stats-for-Biomed/Final Project")
+setwd("~/GitHub/Prob-and-Stats-for-Biomed/Final Project")
 
 # Import TSV files
 clinical <- read.table("Data/clinical.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
 exposure <- read.table("Data/exposure.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-family_history <- read.table("Data/family_history.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-follow_up <- read.table("Data/follow_up.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-aliquot <- read.table("Data/pathology_detail.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-analyte <- read.table("Data/pathology_detail.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-portion <- read.table("Data/pathology_detail.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-sample <- read.table("Data/pathology_detail.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-pathology_detail <- read.table("Data/pathology_detail.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
-slide  <- read.table("Data/slide.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
 mutation <- read.table("Data/frequent-mutations.2024-03-16.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
 genes <- read.table("Data/frequently-mutated-genes.2024-03-16.tsv", header = TRUE, sep = "\t", fill = TRUE, quote = "")
 
@@ -44,17 +36,35 @@ list2env(modified_data_frames, envir = .GlobalEnv)
 
 # filter columns in clinical df
 
-clinical_cleaned = subset(clinical, select = c("case_id", "ethnicity", "gender", "race", "vital_status", "age_at_diagnosis", "ajcc_pathologic_stage", "treatment_or_therapy", "treatment_type"))
+clinical_cleaned = subset(clinical, select = c("case_id", 
+                                               "ethnicity", 
+                                               "gender", 
+                                               "race", 
+                                               "vital_status", 
+                                               "age_at_diagnosis", 
+                                               "days_to_death",
+                                               "ajcc_pathologic_stage", 
+                                               "treatment_or_therapy", 
+                                               "treatment_type"))
 str(clinical_cleaned)
 
 # Convert age column to int and years
 clinical_cleaned$age_at_diagnosis <- as.integer(clinical_cleaned$age_at_diagnosis)
+clinical_cleaned$days_to_death <- as.integer(clinical_cleaned$days_to_death)
 
 clinical_cleaned$age_at_diagnosis <- ifelse(clinical_cleaned$age_at_diagnosis > 150,
                                             clinical_cleaned$age_at_diagnosis / 365,
-                                            round(clinical_cleaned$age_at_diagnosis))
+                                            round(clinical_cleaned$age_at_diagnosis, digits = 2))
+
+# Calculate age at death
+clinical_cleaned <- clinical_cleaned %>% mutate(age_at_death = age_at_diagnosis + days_to_death/365)
+clinical_cleaned <- clinical_cleaned %>% relocate(age_at_death, .after = age_at_diagnosis)
 
 # Remove rows that don't have vital_status
 clinical_cleaned <- subset(clinical_cleaned, !is.na(vital_status))
 
+# Remove rows from exposure that don't have cigarettes
+exposure_cleaned <- subset(exposure, !is.na(cigarettes_per_day))
+
 describe(clinical_cleaned)
+summary(clinical_cleaned)
