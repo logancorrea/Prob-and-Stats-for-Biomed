c7e4e33126209f517bcf5b9a1f2494e5b59a60d9
Logan Correa <48968193+logancorrea@users.noreply.github.com>
2024-04-08 19:30:36 -0600

    commit

--- Final Project/UntitledR.R
+++ Final Project/UntitledR.R
@@ -1,1 +1,6 @@
-rm(list = ls())
+# Get the names of all objects in the environment
+all_objects <- ls()
+
+# Remove all objects except "df"
+objects_to_remove <- setdiff(all_objects, c("part1", "part2", "part3", "part4", "part5", "part6", "genes", "bigdata"))
+rm(list = objects_to_remove)
